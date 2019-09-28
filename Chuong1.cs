c7ee5996ace514b552a656020e04495831e9f92e
Sang <superseasa1@gmail.com>
2019-09-28 13:13:06 +0700

    Update Chuong 1: Bo sung cach khai bao struct

--- Chuong1.cs
+++ Chuong1.cs
@@ -9,4 +9,22 @@ namespace Chuong1
             Console.WriteLine("Hello World");
         }
     }
+
+    //Tạo kiểu cấu trúc - Struct:
+    class KieuCauTruc
+    {
+        public struct st1
+        {
+            public string ten;
+            public int maso;
+        }
+
+        public void KCT()
+        {
+            st1 s;
+            s.ten = "abc";
+            s.maso = 123;
+            Console.WriteLine("Ten: {0} - Ma so: {1}", s.ten, s.maso);
+        }
+    }
 }
